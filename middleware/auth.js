62e858a9179ab737c389a2ed725a6b0d57214733
YuriTadevosyan96 <78906093+YuriTadevosyan96@users.noreply.github.com>
2021-09-28 01:41:20 +0300

    fix gh-pages deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-export default function ({ store, redirect, route }) {
+export default function ({ store, redirect, route, base }) {
   if (!store.state.isAuth) {
     if (route.name !== 'login') {
       if (process.server) {
         return redirect('/login')
       } else {
-        return window.location.replace('/nuxt-contacts/login')
+        return window.location.replace(base + 'login')
       }
     }
   }
